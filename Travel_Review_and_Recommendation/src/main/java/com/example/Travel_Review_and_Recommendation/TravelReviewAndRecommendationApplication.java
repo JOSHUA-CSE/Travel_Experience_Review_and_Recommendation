45f7192062ca38682a1cfc72a169e8bd37de7ad0
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-29 09:48:17 +0530

    feat:Initial commit with Spring Boot Initializer folder structure.

---
+++ Travel_Review_and_Recommendation/src/main/java/com/example/Travel_Review_and_Recommendation/TravelReviewAndRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Travel_Review_and_Recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelReviewAndRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelReviewAndRecommendationApplication.class, args);
+	}
+
+}
---
+++ Travel_Review_and_Recommendation/src/test/java/com/example/Travel_Review_and_Recommendation/TravelReviewAndRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Travel_Review_and_Recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelReviewAndRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
