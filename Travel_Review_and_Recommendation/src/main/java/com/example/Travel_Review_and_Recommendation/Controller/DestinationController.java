bf5f9109b7f8b5bc52fcaa55499594b57634994d
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-30 00:56:44 +0530

    refactor:The destination controller is edited to handle user reviews.

--- Travel_Review_and_Recommendation/src/main/java/com/example/Travel_Review_and_Recommendation/Controller/DestinationController.java
+++ Travel_Review_and_Recommendation/src/main/java/com/example/Travel_Review_and_Recommendation/Controller/DestinationController.java
@@ -2,19 +2,23 @@ package com.example.Travel_Review_and_Recommendation.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.example.Travel_Review_and_Recommendation.Service.DestinationService;
+import com.example.Travel_Review_and_Recommendation.Service.ReviewService;
 import com.example.Travel_Review_and_Recommendation.Service.UserService;
 import com.example.Travel_Review_and_Recommendation.model.Destination;
 import com.example.Travel_Review_and_Recommendation.model.User;
 
-@RestController
+@Controller
 @RequestMapping("/api/destinations")
 public class DestinationController {
     @Autowired
     private DestinationService d_service;
     @Autowired
     private UserService u_service;
+    @Autowired
+    private ReviewService r_service;
     @GetMapping
     public String getAllDestinations(Model model) {
         model.addAttribute("destinations",d_service.getAllDestinations());
@@ -35,11 +39,11 @@ public class DestinationController {
         return "Destination created successfully!";
     }
 
-
     @GetMapping("/{id}")
     public String getAllDestinations(@PathVariable Long id, Model model) {
-        model.addAttribute("Destination",d_service.getDestinationById(id));
-        return "destination/list";
+    model.addAttribute("destination", d_service.getDestinationById(id));
+        model.addAttribute("reviews", r_service.getReviewsByDestinationId(id));
+        return "destination/detail";
     }
     
 }
